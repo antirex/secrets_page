70db24a2591f0e470199155b0c5c94e87ceeaafc
antirex <anshulbamb22@gmail.com>
2020-06-24 21:14:52 +0530

    port changes and tiny mods

--- app.js
+++ app.js
@@ -179,6 +179,6 @@ app.post("/submit", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Your server is live at port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Your server is live and running successfully!");
 });
